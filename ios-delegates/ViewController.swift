e662147b853782d1fea973901331d268601b029d
KAGE <kagemiku@gmail.com>
2016-12-10 23:23:58 +0900

    Initial Commit

---
+++ ios-delegates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-delegates
+//
+//  Created by KAGE on 12/10/16.
+//  Copyright © 2016 KAGE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
