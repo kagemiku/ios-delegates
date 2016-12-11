7346f97e349ebd23ccfd12b526f4552cdbccf2b5
KAGE <kagemiku@gmail.com>
2016-12-11 22:50:08 +0900

    Delete main storyboard file

--- ios-delegates/RootViewController.swift
+++ ios-delegates/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  ios-delegates
 //
 //  Created by KAGE on 12/10/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
